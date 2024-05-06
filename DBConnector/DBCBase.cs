1551f6edacae820ed2d50c82b2979af0aefd3364
Jorge Alberto Aguado Carrillo <jorgea.aguadoc@outlook.com>
2024-05-06 16:37:32 -0600

    Agregar archivos de proyecto.

---
+++ DBConnector/DBCBase.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace DBConnector
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class DBCBase : IDisposable
+	{
+		/// <summary>
+		/// Gets or sets the connection string.
+		/// </summary>
+		/// <value>
+		/// The connection string.
+		/// </value>
+		public string ConnectionString { get; set; }
+		/// <summary>
+		/// Gets or sets a value indicating whether this instance has error.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance has error; otherwise, <c>false</c>.
+		/// </value>
+		public bool HasError { get; set; }
+		/// <summary>
+		/// Gets or sets the error message.
+		/// </summary>
+		/// <value>
+		/// The error message.
+		/// </value>
+		public string ErrorMessage { get; set; }
+
+		private bool disposed = false;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DBCBase"/> class.
+		/// </summary>
+		/// <param name="connectionString">The connection string.</param>
+		public DBCBase(string connectionString)
+		{
+			ConnectionString = connectionString;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DBCBase"/> class.
+		/// </summary>
+		public DBCBase()
+		{
+			
+		}
+
+		/// <summary>
+		/// Sets the error.
+		/// </summary>
+		/// <param name="errorMessage">The error message.</param>
+		public void SetError(string errorMessage)
+		{
+			HasError = true;
+			ErrorMessage = errorMessage;
+		}
+
+		/// <summary>
+		/// Clears the error.
+		/// </summary>
+		public void ClearError()
+		{
+			HasError = false;
+			ErrorMessage = string.Empty;
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					ConnectionString = null;
+					ErrorMessage = null;
+				}
+
+				disposed = true;
+			}
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+
+	/// <summary>
+	///   <br />
+	/// </summary>
+	public class ParameterList
+	{
+		/// <summary>
+		/// Gets or sets the Parameter Key.
+		/// </summary>
+		/// <value>
+		/// The Parameter Key.
+		/// </value>
+		public string Key { get; set; }
+		/// <summary>
+		/// Gets or sets the Parameter Value.
+		/// </summary>
+		/// <value>
+		/// The Parameter Value.
+		/// </value>
+		public string Value { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ParameterList"/> class.
+		/// </summary>
+		/// <param name="ParameterKey">The Parameter Key.</param>
+		/// <param name="ParameterValue">The Parameter Value.</param>
+		public ParameterList(string ParameterKey, string ParameterValue)
+		{
+			Key = ParameterKey;
+			Value = ParameterValue;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ParameterList"/> class.
+		/// </summary>
+		public ParameterList()
+		{
+		}
+	}
+
+}
---
+++ SQLConnector/SQLConnector.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DBConnector
+{
+	public class SQLConnector : DBCBase
+	{
+
+		public SQLConnector(string connectionString) : base(connectionString)
+		{
+		}
+
+		public SQLConnector()
+		{
+		}
+
+		public DataTable GetDataTable(string StoredProcedure)
+		{
+			try
+			{
+				ClearError();
+
+				DataTable dt = new DataTable();
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					using (SqlCommand cmd = new SqlCommand(StoredProcedure, conn))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+						{
+							da.Fill(dt);
+						}
+						
+					}
+				}
+				return dt;
+			}
+			catch (Exception ex)
+			{
+				SetError(ex.Message);
+				return null;
+			}
+			
+		}
+
+		public string GetString(string StoredProcedure)
+		{
+			try
+			{
+				ClearError();
+
+				string result = "";
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					using (SqlCommand cmd = new SqlCommand(StoredProcedure, conn))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						result = cmd.ExecuteScalar().ToString();
+					}
+				}
+				return result;
+			}
+			catch (Exception ex)
+			{
+				SetError(ex.Message);
+				return null;
+			}
+		}
+
+		public int ExecuteNonQuery(string StoredProcedure)
+		{
+			try
+			{
+				ClearError();
+
+				int result = 0;
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					using (SqlCommand cmd = new SqlCommand(StoredProcedure, conn))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						result = cmd.ExecuteNonQuery();
+					}
+				}
+				return result;
+			}
+			catch (Exception ex)
+			{
+				SetError(ex.Message);
+				return -1;
+			}
+		}
+
+		public int GetInt(string StoredProcedure)
+		{
+			try
+			{
+				ClearError();
+
+				int result = 0;
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					using (SqlCommand cmd = new SqlCommand(StoredProcedure, conn))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						result = Convert.ToInt32(cmd.ExecuteScalar());
+					}
+				}
+				return result;
+			}
+			catch (Exception ex)
+			{
+				SetError(ex.Message);
+				return -1;
+			}
+		}
+
+		public decimal GetDecimal(string StoredProcedure)
+		{
+			try
+			{
+				ClearError();
+
+				decimal result = 0;
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					using (SqlCommand cmd = new SqlCommand(StoredProcedure, conn))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						result = Convert.ToDecimal(cmd.ExecuteScalar());
+					}
+				}
+				return result;
+			}
+			catch (Exception ex)
+			{
+				SetError(ex.Message);
+				return -1;
+			}
+		}
+
+	}
+}
